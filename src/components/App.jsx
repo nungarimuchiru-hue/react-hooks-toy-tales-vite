e0b6d5ce915253087def9a445e4de8a92ce4e347
Nungari Muchiru <nungari@Nungari.localdomain>
2026-05-15 05:13:40 +0300

    Add changes made to App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,73 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
 function App() {
+  // state for showing or hiding the form
   const [showForm, setShowForm] = useState(false);
 
+  // state for storing toys
+  const [toys, setToys] = useState([]);
+
+  // fetch toys when the app loads
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then((response) => response.json())
+      .then((data) => {
+        setToys(data);
+      });
+  }, []);
+
+  // toggle form
   function handleClick() {
-    setShowForm((showForm) => !showForm);
+    setShowForm(!showForm);
+  }
+
+  // add new toy
+  function handleAddToy(newToy) {
+    setToys([...toys, newToy]);
+  }
+
+  // update toy likes
+  function handleUpdateToy(updatedToy) {
+    const newToyList = toys.map((toy) => {
+      if (toy.id === updatedToy.id) {
+        return updatedToy;
+      } else {
+        return toy;
+      }
+    });
+
+    setToys(newToyList);
+  }
+
+  // delete toy
+  function handleDeleteToy(id) {
+    const remainingToys = toys.filter((toy) => toy.id !== id);
+
+    setToys(remainingToys);
   }
 
   return (
-    <>
+    <div>
       <Header />
-      {showForm ? <ToyForm /> : null}
+
       <div className="buttonContainer">
-        <button onClick={handleClick}>Add a Toy</button>
+        <button onClick={handleClick}>
+          {showForm ? "Hide Form" : "Add a Toy"}
+        </button>
       </div>
-      <ToyContainer />
-    </>
+
+      {showForm && <ToyForm onAddToy={handleAddToy} />}
+
+      <ToyContainer
+        toys={toys}
+        onUpdateToy={handleUpdateToy}
+        onDeleteToy={handleDeleteToy}
+      />
+    </div>
   );
 }
 
